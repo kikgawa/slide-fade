c71ed03caa59328260dac3836cbabf3f63798cba
Minori Kikugawa <minocolor@gmail.com>
2022-07-06 19:07:33 +0900

    prev nextボタン スライド切り替え

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
-const slides = document.querySelectorAll('.slide-item');
-const slidesLength = slides.length;
-let now = 0;
+const slides = document.querySelectorAll('.slide-item')
+const slideLength = slides.length
+const slideNavs = document.querySelectorAll('.slide-nav')
+const slidePerv = document.querySelector('.slide-nav-prev')
+const slideNext = document.querySelector('.slide-nav-next')
+let now = 0
+let intervalTime = 6000
 
 window.addEventListener('load', () => {
   slides[0].classList.add('active')
@@ -8,11 +12,29 @@ window.addEventListener('load', () => {
 
 const slideFade = () => {
   slides[now].classList.remove('active')
-  if (now >= slidesLength - 1) {
+  if (now >= slideLength - 1) {
     now = 0
   } else {
     now++
   }
-  slides[now].classList.add('active');
+  slides[now].classList.add('active')
 }
-setInterval(slideFade, 6000)
+// (() => {
+//   timeoutId = setInterval(slideFade, intervalTime)
+// })();
+setInterval(slideFade, intervalTime)
+
+slidePerv.addEventListener('click', () => {
+  slides[now].classList.remove('active')
+  if (now == 0) {
+    now = slideLength - 1
+  } else {
+    now--
+  }
+  slides[now].classList.add('active')
+  // clearInterval(timeoutId)
+})
+slideNext.addEventListener('click', () => {
+  slideFade()
+  // clearInterval(timeoutId)
+})
