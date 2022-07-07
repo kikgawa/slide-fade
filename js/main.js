d4c8c1995bfa2f83f844cc43167d76793455c524
Minori Kikugawa <minocolor@gmail.com>
2022-07-07 14:11:23 +0900

    slide 同一ページ複数対応

--- js/main.js
+++ js/main.js
@@ -1,39 +1,50 @@
-const slides = document.querySelectorAll('.slide-item')
-const slideLength = slides.length
-const slidePerv = document.querySelector('.slide-nav-prev')
-const slideNext = document.querySelector('.slide-nav-next')
-let now = 0
-let intervalTime = 6000
+const createSlideShow = (targetId) => {
+  const slides = document.querySelectorAll(`${targetId} .slide-item`)
+  const slideLength = slides.length
+  const slidePrev = document.querySelector(`${targetId} .slide-nav-prev`)
+  const slideNext = document.querySelector(`${targetId} .slide-nav-next`)
+  let now = 0
+  let intervalTime = 6000
+  const slideShow = {
+    play() {
+      window.addEventListener('load', () => {
+        slides[0].classList.add('active')
+      })
 
-window.addEventListener('load', () => {
-  slides[0].classList.add('active')
-})
-
-const slideFade = () => {
-  slides[now].classList.remove('active')
-  if (now >= slideLength - 1) {
-    now = 0
-  } else {
-    now++
+      const slideFade = () => {
+        slides[now].classList.remove('active')
+        if (now >= slideLength - 1) {
+          now = 0
+        } else {
+          now++
+        }
+        slides[now].classList.add('active')
+      }
+      slidePrev.addEventListener('click', () => {
+        slides[now].classList.remove('active')
+        if (now == 0) {
+          now = slideLength - 1
+        } else {
+          now--
+        }
+        slides[now].classList.add('active')
+        clearInterval(timer)
+        timer = setInterval(slideFade, intervalTime)
+      })
+      slideNext.addEventListener('click', () => {
+        slideFade()
+        clearInterval(timer)
+        timer = setInterval(slideFade, intervalTime)
+      })
+      let timer = setInterval(slideFade, intervalTime)
+    }
   }
-  slides[now].classList.add('active')
+  return slideShow
 }
 
-slidePerv.addEventListener('click', () => {
-  slides[now].classList.remove('active')
-  if (now == 0) {
-    now = slideLength - 1
-  } else {
-    now--
-  }
-  slides[now].classList.add('active')
-  clearInterval(timer)
-  timer = setInterval(slideFade, intervalTime)
-})
-slideNext.addEventListener('click', () => {
-  slideFade()
-  clearInterval(timer)
-  timer = setInterval(slideFade, intervalTime)
-})
-
-let timer = setInterval(slideFade, intervalTime)
+const slide1 = createSlideShow('#slide-1')
+const slide2 = createSlideShow('#slide-2')
+const slide3 = createSlideShow('#slide-3')
+slide1.play()
+slide2.play()
+slide3.play()
